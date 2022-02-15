3564fda0cce4dd9c1a8a261bbd8dc25944ea969f
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-02-15 15:22:51 +0300

    added services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
