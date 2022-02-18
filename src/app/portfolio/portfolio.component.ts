2d7aa6587f11e0dd5d7a15a9dd80759be7015365
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-02-18 14:03:06 +0300

    fix

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { Navigation, SwiperOptions } from 'swiper';
 
+SwiperCore.use([Navigation]);
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
@@ -11,5 +13,11 @@ export class PortfolioComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+  };
 }
