15bf548b78b5397e63af7f328c6494c16709a288
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-02-21 12:27:31 +0300

    fix router

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  mobile = false;
+  constructor(private viewportscroller: ViewportScroller) { }
 
   ngOnInit(): void {
+    if (window.screen.width <= 968) {
+      // 768px portrait
+      this.mobile = true;
+      console.log('mobile', this.mobile);
+    }
   }
-
+  onClickScroll(elementId: string): void {
+    this.viewportscroller.scrollToAnchor(elementId);
+  }
 }
