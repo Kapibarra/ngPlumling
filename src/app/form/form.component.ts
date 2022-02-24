d056bf27cedb7c22c561a829d787f511caea4f46
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-02-24 12:07:15 +0300

    fixes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,28 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Mail } from './form'
+import { Mail } from './form';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-mail!: Mail;
-  constructor(private http: HttpClient) { }
+  mail!: Mail;
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit(formValue: any) {
+    var formData = new FormData();
+    for (var key in formValue) {
+      formData.append(key, formValue[key]);
+    }
+    this.http
+      .post('/assets/handler.php', formData)
+      .subscribe((responce: any) => {
+        console.log(responce);
+      });
   }
-onSubmit(formValue: any) {
-  const formData = new FormData(formValue);
-  
-  console.log(formValue);
-  console.log(formData);
-  
-  this.http.post('/assets/handler.php', formValue).subscribe((responce: any) => {
-  
-  console.log(responce);
-
-  })
-}
 }
